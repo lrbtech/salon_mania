42debff388fe4dace0a86a109d16064fa6c6881c
unknown <info@lrbtech.com>
2021-09-04 15:09:09 +0400

    dep v2

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2283,7 +2283,7 @@ if(count($coupon)>0){
         $data =array();
         if(count($package) >0){
             foreach ($package as $key => $value) {
-                $pack = product::find($value->package_id);
+                $pack = product::find($value->product_id);
                 $data = array(
                     'product_id' => $value->product_id,
                     'product_name' => $value->product_name,
